a042adc66885b4dcefa6298595e5b9023e87b6aa
Pete Smith <roysvork@gmail.com>
2013-06-02 18:58:21 +0100

    Added unit test project

---
+++ Superscribe/Utils/SuperList.cs
@@ -0,0 +1,20 @@
+﻿namespace Superscribe.Utils
+{
+    using System.Collections.Generic;
+
+    using Superscribe.Models;
+
+    public class SuperList : List<SuperscribeState>
+    {
+        public static SuperscribeState operator /(string name, SuperList others)
+        {
+            var state = new ConstantState(name);
+            foreach (var s in others)
+            {
+                state.Slash(s);
+            }
+
+            return state;
+        }
+    }
+}
