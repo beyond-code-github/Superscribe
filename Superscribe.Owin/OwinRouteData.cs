a9abf4339050a8805263a12010fc353cb3478768
Pete Smith <roysvork@gmail.com>
2013-08-22 17:03:08 +0100

    Added Superscribe.Owin project which allows the user of Superscribe with OWIN.

---
+++ Superscribe.Owin/OwinRouteData.cs
@@ -0,0 +1,13 @@
+﻿namespace Superscribe.Owin
+{
+    using Superscribe.Models;
+
+    using global::Owin.Types;
+
+    public class OwinRouteData : RouteData
+    {
+        public OwinRequest Request { get; set; }
+
+        public OwinResponse Response { get; set; }
+    }
+}
---
+++ Superscribe.Owin/StartupExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Superscribe.Owin
+{
+    using Superscribe.Models;
+    using Superscribe.Utils;
+
+    using global::Owin;
+
+    public static class StartupExtensions
+    {
+        public static IAppBuilder UseSuperscribe(
+            this IAppBuilder builder)
+        {
+            return builder.UseHandlerAsync((req, res) =>
+                {
+                    var path = req.Path;
+                    var routeData = new RouteData();
+
+                    var walker = new RouteWalker(ʃ.Base);
+                    walker.WalkRoute(path, routeData);
+
+                    if (walker.IncompleteMatch)
+                    {
+                        res.StatusCode = 404;
+                        return res.WriteAsync("404 - Route was incomplete");
+                    }
+
+                    if (walker.ExtraneousMatch)
+                    {
+                        res.StatusCode = 404;
+                        return res.WriteAsync("404 - Route match failed");
+                    }
+
+                    res.ContentType = "text/html";
+                    return res.WriteAsync(routeData.Response);
+                });
+        }
+    }
+}
