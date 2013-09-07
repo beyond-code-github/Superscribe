0377f8bf311d695d633c42e07286d31e611a3b74
Pete Smith <roysvork@gmail.com>
2013-09-07 12:12:35 +0100

    Added skeleton base class for Superscribe module style routes

---
+++ Superscribe/SuperscribeModule.cs
@@ -0,0 +1,34 @@
+﻿namespace Superscribe
+{
+    using System;
+
+    using Superscribe.Models;
+
+    public class SuperscribeModule
+    {
+        protected void Get(Func<SuperscribeNode, RouteGlue, SuperscribeNode> config)
+        {
+            
+        }
+
+        protected void Post(Func<SuperscribeNode, RouteGlue, SuperscribeNode> config)
+        {
+
+        }
+
+        protected void Put(Func<SuperscribeNode, RouteGlue, SuperscribeNode> config)
+        {
+
+        }
+
+        protected void Delete(Func<SuperscribeNode, RouteGlue, SuperscribeNode> config)
+        {
+
+        }
+
+        protected void Patch(Func<SuperscribeNode, RouteGlue, SuperscribeNode> config)
+        {
+
+        }
+    }
+}
