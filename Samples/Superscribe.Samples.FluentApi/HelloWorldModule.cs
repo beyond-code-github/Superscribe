7f0cab52a0115b4c9db1aabc7b0de5c4825698c2
Pete Smith <roysvork@gmail.com>
2013-11-09 16:58:59 +0000

    Added fluent configuration sample project

---
+++ Samples/Superscribe.Samples.FluentApi/HelloWorldModule.cs
@@ -0,0 +1,23 @@
+﻿namespace Superscribe.Samples.FluentApi
+{
+    using Superscribe.Models;
+    using Superscribe.Owin;
+
+    public class HelloWorldModule : SuperscribeOwinModule
+    {
+        public HelloWorldModule()
+        {
+            ʃ.Base.FinalFunctions.Add(new FinalFunction("GET", _ => @"
+                Welcome to Superscribe 
+                <a href='/Hello/World'>Say hello...</a>
+            "));
+
+            var helloRoute = new ConstantNode("Hello").Slash(new ConstantNode("World"));
+            helloRoute.FinalFunctions.Add(new FinalFunction("GET", _ => "Hello World"));
+
+            var justHelloRoute = new ConstantNode("Hello").Optional();
+            
+            ʃ.Base.Zip(helloRoute.Base());
+        }
+    }
+}
---
+++ Samples/Superscribe.Samples.FluentApi/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace Superscribe.Samples.FluentApi
+{
+    using System.IO;
+
+    using global::Owin;
+
+    using Newtonsoft.Json;
+
+    using Superscribe.Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new SuperscribeOwinConfig();
+            config.MediaTypeHandlers.Add(
+                "application/json",
+                new MediaTypeHandler
+                    {
+                        Write = (res, o) => res.WriteAsync(JsonConvert.SerializeObject(o)),
+                        Read = (req, type) =>
+                            {
+                                object obj;
+                                using (var reader = new StreamReader(req.Body))
+                                {
+                                    obj = JsonConvert.DeserializeObject(reader.ReadToEnd(), type);
+                                };
+
+                                return obj;
+                            }
+                    });
+
+            config.MediaTypeHandlers.Add(
+                "text/html",
+                new MediaTypeHandler
+                    {
+                        Write = (res, o) => res.WriteAsync(o.ToString())
+                    });
+
+            app.UseSuperscribeModules(config);
+        }
+    }
+}
