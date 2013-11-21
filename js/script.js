e43aba5100fb5e1a676919fff01db0f542478640
Pete Smith <roysvork@gmail.com>
2013-11-21 20:52:20 +0000

    Enabled hashchange for tabs

--- js/script.js
+++ js/script.js
@@ -265,4 +265,15 @@ $(document).ready(function() {
     });
   });
   
+  var hash = document.location.hash;
+  var prefix = "tab_";
+  if (hash) {
+      $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+  } 
+
+  // Change hash for page-reload
+  $('.nav-tabs a').on('shown.bs.tab', function (e) {
+      window.location.hash = e.target.hash.replace("#", "#" + prefix);
+  });
+
 });
