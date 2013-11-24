72084d4aac0d9f2c3f788082fe8d6095b355be75
Pete Smith <roysvork@gmail.com>
2013-11-24 18:38:05 +0000

    Added full pipeline test suite for Owin bits

---
+++ Superscribe.Tests.Owin/Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace Superscribe.Tests.Owin
+{
+    using System.Linq;
+    using System.Net.Http;
+
+    using Machine.Specifications;
+
+    using Microsoft.Owin.Testing;
+
+    public class Tests
+    {
+        protected static TestServer owinTestServer;
+
+        protected static HttpResponseMessage responseMessage;
+
+        protected static HttpClient client;
+
+        protected Establish context = () =>
+            {
+                owinTestServer = TestServer.Create(builder => new Startup().Configuration(builder));
+
+                client = owinTestServer.HttpClient;
+                client.DefaultRequestHeaders.Add("accept", "text/html");
+            };
+
+        private Because of = () =>
+                responseMessage = client.GetAsync("http://localhost/").Result;
+
+        private It should_add_the_before_header =
+            () => responseMessage.Headers.FirstOrDefault(o => o.Key == "before").Value.ShouldContain("before");
+
+        private It should_set_the_response =
+           () => responseMessage.Content.ReadAsStringAsync().Result.ShouldEqual("Hello World");
+
+        private It should_add_the_after_header =
+            () => responseMessage.Headers.FirstOrDefault(o => o.Key == "after").Value.ShouldContain("after");
+    }
+}
