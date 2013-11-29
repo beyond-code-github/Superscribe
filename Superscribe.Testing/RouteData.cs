49f7f93695cff5aafcf8878ec27e9b547332724c
Pete Smith <roysvork@gmail.com>
2013-11-29 20:48:09 +0000

    Removed web api specific fluff from test implementation of routedata

--- Superscribe.Testing/RouteData.cs
+++ Superscribe.Testing/RouteData.cs
@@ -5,47 +5,13 @@
 
     public class RouteData : IRouteData
     {
-        private string actionName;
-
-        private string controllerName;
-
         public RouteData()
         {
             this.Parameters = new DynamicDictionary();
         }
         
         public dynamic Parameters { get; set; }
-
-        public bool ActionNameSpecified { get; private set; }
-
-        public string ActionName
-        {
-            get
-            {
-                return this.actionName;
-            }
-            set
-            {
-                this.ActionNameSpecified = true;
-                this.actionName = value;
-            }
-        }
-
-        public bool ControllerNameSpecified { get; private set; }
-
-        public string ControllerName
-        {
-            get
-            {
-                return this.controllerName;
-            }
-            set
-            {
-                this.ControllerNameSpecified = true;
-                this.controllerName = value;
-            }
-        }
-
+        
         public object Response { get; set; }
 
         public bool ParamConversionError { get; set; }
