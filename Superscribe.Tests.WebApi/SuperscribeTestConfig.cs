07afa707ab28fa1342e92cebb263cfd2129cc062
Pete Smith <roysvork@gmail.com>
2014-02-23 00:31:49 +0000

    Beautification of test configuration to match the website

--- Superscribe.Tests.WebApi/SuperscribeTestConfig.cs
+++ Superscribe.Tests.WebApi/SuperscribeTestConfig.cs
@@ -18,21 +18,26 @@
 
             engine.Route("Any" / Any.Controller / Any.Action / (Int)"siteId");
 
-            var forms = engine.Route(r => r / "api" / (Long)"parentId" / "Forms".Controller());
-            var blog = engine.Route(r => r / "sites" / (Int)"siteId" / "blog");
-            var portfolio = engine.Route(r => r / "sites" / (Int)"siteId" / "portfolio");
+            var forms       = engine.Route(r => r / "api" / (Long)"parentId" / "Forms".Controller());
+            var blog        = engine.Route(r => r / "sites" / (Int)"siteId" / "blog");
+            var portfolio   = engine.Route(r => r / "sites" / (Int)"siteId" / "portfolio");
 
-            var blogposts = engine.Route(blog / "posts".Controller("blogposts"));
+            var blogposts   = engine.Route(blog / "posts".Controller("blogposts"));
 
             engine.Route(forms / "VisibleFor".Action() / (String)"appDataId");
             engine.Route(forms / -(Long)"id");
             
             engine.Route(blog / "tags".Controller("blogtags"));
             
-            engine.Route(blogposts / -(Int)"postId" / -"media".Controller("blogpostmedia") / -(Int)"id");
-            engine.Route(blogposts / "archives".Controller("blogpostarchives") / -(Int)"year" / (Int)"month");
+            engine.Route(blogposts / -(Int)"postId" / -"media".Controller("blogpostmedia") 
+                / -(Int)"id");
 
-            engine.Route(portfolio / "projects".Controller("portfolioprojects") / -(Int)"projectId" / -"media".Controller("portfolioprojectmedia") / -(Int)"id");
+            engine.Route(blogposts / "archives".Controller("blogpostarchives") / -(Int)"year" 
+                / (Int)"month");
+
+            engine.Route(portfolio / "projects".Controller("portfolioprojects") 
+                / -(Int)"projectId" / -"media".Controller("portfolioprojectmedia") / -(Int)"id");
+
             engine.Route(portfolio / "tags".Controller("portfoliotags"));
             engine.Route(portfolio / "categories".Controller("portfoliocategories") / -(Int)"id");
         }
